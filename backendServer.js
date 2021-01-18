a46f8e918467a5d0d96e4573c3ef5713946cfce6
Martin Smitnya <smitnyamartin@gmail.com>
2021-01-18 10:58:53 +0100

    post request and get:id done

--- backendServer.js
+++ backendServer.js
@@ -26,7 +26,7 @@ app.get ('/', (req, res) => {
   res.send('MAIN PAGE CONNECTED');
 })
 
-app.get('/databasePROBA', (req, res) => {
+app.get('/posts', (req, res) => {
   conn.query('SELECT * FROM posts_table;', (err, rows) => {
     if (err) {
       res.status(500).json({error: 'Database error occured'});
@@ -37,10 +37,34 @@ app.get('/databasePROBA', (req, res) => {
   })
 })
 
-//Get posts endpoint  /posts
- 
-//Get uniq post endpoint /posts/:id
-//Add posts endpoint  /postsAdd/ ??? (maybe we need PUT /posts and separate GET /posts)
+app.get('/posts/:id', (req, res) => {
+  conn.query(`SELECT * FROM posts_table WHERE id = ?;`, [req.params.id] , (err, rows) => {
+    if (err) {
+      res.status(500).json({error: 'Database error occured'});
+      return
+    } 
+    else if (rows[0] === undefined) {
+      res.status(404).json({error: `Post with given ID does not exists`});
+    } 
+    else {
+      res.json(rows);
+    }
+  })
+})
+
+//POST request, input body = [{"title":"otherDummyTtitle","url":"www.dummy.com","timestamp":333}], ID andscore auto generated
+app.post ('/posts', (req, res) => {
+  conn.query(`INSERT INTO posts_table (title, url, timestamp) VALUES (?, ?, ?);`, [req.body[0].title, req.body[0].url, req.body[0].timestamp], (err, rows) => {
+    if (err) {
+      res.status(500).json({error: 'Database error occured'});
+      return
+    } else {
+      res.json('Inserted!');
+    }
+  });
+});
+
+//Add posts endpoint  /posts
 //Downvote endpoint /posts/:id/downvote
 //Upvote   endpoint /posts/:id/upvote
 
@@ -52,3 +76,8 @@ app.get('/databasePROBA', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is ONLINE at http://localhost:3000/'); 
 })
+
+
+
+//Get posts endpoint  /posts
+//Get uniq post endpoint /posts/:id
